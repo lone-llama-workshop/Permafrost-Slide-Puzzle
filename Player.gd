9b7acf24a6d2988b37339a711f83f9b885ea6fd4
Josh Maxwell <cctechwiz@gmail.com>
2020-07-13 13:23:49 -0600

    Basic grid based movement is implemented

--- Player.gd
+++ Player.gd
@@ -2,12 +2,31 @@ extends Area2D
 
 const GRID_SIZE: int = 64
 
+onready var grid: TileMap = get_parent()
+
 
 func _ready() -> void:
-	position = position.snapped(Vector2.ONE * GRID_SIZE)
-	#position = Vector2.ZERO
-	position += Vector2.ONE * GRID_SIZE/2
+	print(grid.cell_size)
+	print(grid.world_to_map(position))
+	
+	position = grid.world_to_map(position) + (grid.cell_size / 2)
+	print(position)
+	
+	print(grid.get_used_cells())
+	print(grid.get_cellv(grid.world_to_map(position)))
 
 
 func _process(delta: float) -> void:
-	pass
+	var input_direction = get_input_direction()
+	if not input_direction:
+		return
+		
+	print(input_direction)
+	position += grid.cell_size * input_direction
+
+
+func get_input_direction() -> Vector2:
+	return Vector2(
+		int(Input.is_action_just_pressed("ui_right")) - int(Input.is_action_just_pressed("ui_left")),
+		int(Input.is_action_just_pressed("ui_down")) - int(Input.is_action_just_pressed("ui_up"))
+	)
